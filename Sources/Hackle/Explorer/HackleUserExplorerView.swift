71d222316c3ecfc936e14c99e1b74302f622a6b6
yong <yong@hackle.io>
2023-03-31 00:09:24 +0900

    package

--- Sources/Hackle/Explorer/HackleUserExplorerView.swift
+++ Sources/Hackle/Explorer/HackleUserExplorerView.swift
@@ -39,22 +39,11 @@ class HackleUserExplorerView {
 
     private func getKeyWindow() -> UIWindow? {
         if #available(iOS 13, *) {
-            return UIApplication.shared.connectedScenes
-                .filter {
-                    $0.activationState == .foregroundActive
-                }
-                .compactMap {
-                    $0 as? UIWindowScene
-                }
-                .first?.windows
-                .filter {
-                    $0.isKeyWindow
-                }
-                .first
-        } else {
             return UIApplication.shared.windows.first { window in
                 window.isKeyWindow
             }
+        } else {
+            return UIApplication.shared.keyWindow
         }
     }
 
