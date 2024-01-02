1787dfad0beacec953624b1fd1485b4d4ed20a1d
Wontak Kim <wontak@hackle.io>
2024-01-02 15:03:06 +0900

    [HACKLE-9107] 딥링크 처리 추가

--- Sources/Hackle/UI/Notification/NotificationHandler.swift
+++ Sources/Hackle/UI/Notification/NotificationHandler.swift
@@ -16,5 +16,24 @@ class NotificationHandler {
     
     static func handleNotificationData(data: NotificationData, timestamp: Date = Date()) {
         receiver.onNotificationDataReceived(data: data, timestamp: timestamp)
+        trampoline(data: data)
+    }
+}
+
+extension NotificationHandler {
+    private static func trampoline(data: NotificationData) {
+        switch (data.clickAction) {
+        case .APP_OPEN:
+            break;
+        case .DEEP_LINK:
+            if let link = data.link,
+               let url = URL(string: link) {
+                UIUtils.application.open(url, options: [:]) { success in
+                    Log.debug("Redirected to: \(link) [success=\(success)]")
+                }
+            } else {
+                Log.debug("Landing url is empty.")
+            }
+        }
     }
 }
