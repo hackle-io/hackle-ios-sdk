d54b4748cfb629ecf091e6c671d01e3fe9e3c8b5
Wontak Kim <wontak@hackle.io>
2024-01-09 11:27:12 +0900

    [HACKLE-9107] 이미지 확장자가 함께 attachment에 포함되도록 수정

--- Sources/Hackle/UI/Notification/NotificationFactory.swift
+++ Sources/Hackle/UI/Notification/NotificationFactory.swift
@@ -10,20 +10,23 @@ extension UNNotificationRequest {
               let imageData = try? Data(contentsOf: imageUrl) else {
             return nil
         }
-        return try? UNNotificationAttachment(data: imageData, options: nil)
+        return try? UNNotificationAttachment(
+            identifier: imageUrl.lastPathComponent,
+            data: imageData,
+            options: nil
+        )
     }
 }
 
 fileprivate extension UNNotificationAttachment {
-    convenience init(data: Data, options: [NSObject: AnyObject]?) throws {
+    convenience init(identifier: String, data: Data, options: [NSObject: AnyObject]?) throws {
         let fileManager = FileManager.default
         let tempDirName = ProcessInfo.processInfo.globallyUniqueString
         let tempDirUrl = URL(fileURLWithPath: NSTemporaryDirectory())
             .appendingPathComponent(tempDirName, isDirectory: true)
         try fileManager.createDirectory(at: tempDirUrl, withIntermediateDirectories: true, attributes: nil)
-        let imageFileIdentifier = UUID().uuidString
-        let fileURL = tempDirUrl.appendingPathComponent(imageFileIdentifier)
+        let fileURL = tempDirUrl.appendingPathComponent(identifier)
         try data.write(to: fileURL)
-        try self.init(identifier: imageFileIdentifier, url: fileURL, options: options)
+        try self.init(identifier: identifier, url: fileURL, options: options)
     }
 }
