7253bf67e50d1df604cb4b5a35e555b5d32cf684
Wontak Kim <wontak@hackle.io>
2024-01-22 18:53:43 +0900

    [HACKLE-9356] workspace config repository 클래스 추가

---
+++ Sources/Hackle/Core/Workspace/repository/WorkspaceConfigRepository.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+protocol WorkspaceConfigRepository {
+    func get() -> WorkspaceConfig?
+    func set(value: WorkspaceConfig)
+}
+
+class DefaultWorkspaceConfigRepository: WorkspaceConfigRepository {
+    private let file: FileReadWriter?
+    
+    init(file: FileReadWriter?) {
+        self.file = file
+    }
+    
+    func get() -> WorkspaceConfig? {
+        if let data = try? file?.read(),
+           let config = try? JSONDecoder().decode(WorkspaceConfig.self, from: data) {
+            return config
+        } else {
+            return nil
+        }
+    }
+    
+    func set(value: WorkspaceConfig) {
+        if let data = try? JSONEncoder().encode(value) {
+            try? file?.write(data: data)
+        }
+    }
+}
+
+fileprivate extension DefaultWorkspaceConfigRepository {
+    private static let FILE_NAME = "workspace.json"
+}
---
+++ Tests/HackleTests/Core/Workspace/DefaultWorkspaceConfigRepositorySpecs.swift
@@ -0,0 +1,56 @@
+import Foundation
+import Quick
+import Nimble
+@testable import Hackle
+
+class DefaultWorkspaceConfigRepositorySpecs: QuickSpec {
+    func readTextFromRes(filename: String) -> String {
+        return try! String(contentsOfFile: Bundle(for: DefaultWorkspaceConfigRepositorySpecs.self).path(forResource: filename, ofType: "json")!)
+    }
+    
+    override func spec() {
+        it("get") {
+            let initialData = self.readTextFromRes(filename: "workspace_config")
+            let mockFile = MockFile(initialData: initialData)
+            let repository = DefaultWorkspaceConfigRepository(file: mockFile)
+            
+            expect(repository.get()?.lastModified) == "Tue, 16 Jan 2024 07:39:44 GMT"
+            expect(repository.get()?.config.workspace.id) == 3
+            expect(repository.get()?.config.workspace.environment.id) == 5
+        }
+        
+        it("set") {
+            let mockFile = MockFile()
+            let repository = DefaultWorkspaceConfigRepository(file: mockFile)
+            
+            expect(repository.get()).to(beNil())
+            
+            let json = self.readTextFromRes(filename: "workspace_config")
+            let data = try! JSONDecoder().decode(WorkspaceConfig.self, from: json.data(using: .utf8)!)
+            repository.set(value: data)
+            
+            expect(repository.get()?.lastModified) == "Tue, 16 Jan 2024 07:39:44 GMT"
+        }
+        
+        it("overwrite") {
+            let initialData = self.readTextFromRes(filename: "workspace_config")
+            let mockFile = MockFile(initialData: initialData)
+            let repository = DefaultWorkspaceConfigRepository(file: mockFile)
+            
+            expect(repository.get()?.lastModified) == "Tue, 16 Jan 2024 07:39:44 GMT"
+            
+            let modifiedData = self.readTextFromRes(filename: "workspace_config_modified")
+            let data = try! JSONDecoder().decode(WorkspaceConfig.self, from: modifiedData.data(using: .utf8)!)
+            repository.set(value: data)
+            
+            expect(repository.get()?.lastModified) == "Mon, 22 Jan 2024 08:37:33 GMT"
+        }
+        
+        it("invalid json text exists") {
+            let mockFile = MockFile(initialData: "{!")
+            let repository = DefaultWorkspaceConfigRepository(file: mockFile)
+            
+            expect(repository.get()).to(beNil())
+        }
+    }
+}
