afca62060ae4ccce5499ce94dd8858e5847f1d34
Yong Kim <95567761+yong-hackle@users.noreply.github.com>
2024-03-11 14:33:39 +0900

    support objc notification (#167)
    support objc notification

--- Hackle.podspec
+++ Hackle.podspec
@@ -8,7 +8,7 @@ spec.license       = { :type => "Apache License, Version 2.0", :file => "LICENSE
 spec.author        = { "Hackle" => "platform@hackle.io" }
 spec.ios.deployment_target = "10.0"
 spec.source        = { :git => "https://github.com/hackle-io/hackle-ios-sdk.git", :tag => "#{spec.version}" }
-spec.source_files  = "Sources/**/*.swift"
+spec.source_files  = "Sources/**/*.{swift,h,m}"
 spec.resources     = "Sources/**/*.{xib,png}"
 spec.frameworks    = "Foundation"
 spec.swift_version = "5.0"
---
+++ Sources/Hackle/HackleNotification.h
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+#import <UserNotifications/UserNotifications.h>
+
+@interface HackleNotification : NSObject
+
++ (void)setPushToken:(NSData *)deviceToken;
+
++ (BOOL)userNotificationCenter:(UNUserNotificationCenter *)center
+        willPresentNotification:(UNNotification *)notification
+        withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler;
+
++ (BOOL)userNotificationCenter:(UNUserNotificationCenter *)center
+        didReceiveResponse:(UNNotificationResponse *)response
+        withCompletionHandler:(void (^)(void))completionHandler;
+
+@end
