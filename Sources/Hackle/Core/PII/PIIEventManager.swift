e2d639f4c57ee850f15723b24fa7f3065c235784
sungwoo-hackle <sungwoo.yeo@hackle.io>
2025-04-02 13:35:47 +0900

    주석 추가

--- Sources/Hackle/Core/PII/PIIEventManager.swift
+++ Sources/Hackle/Core/PII/PIIEventManager.swift
@@ -7,8 +7,20 @@
 
 import Foundation
 
+/// Personally Identifiable Information Event Manager
 protocol PIIEventManager {
+    /// 전화번호를 설정
+    ///
+    /// set 할 때마다 기존 전화번호는 덮어쓰기 됩니다.
+    /// - Parameters:
+    ///   - phoneNumber: 전화번호
+    ///   - user: 유저
+    ///   - timestamp: 설정 시간
     func setPhoneNumber(phoneNumber: String, user: User, timestamp: Date)
+    /// 전화번호를 삭제
+    /// - Parameters:
+    ///   - user: 유저
+    ///   - timestamp: 삭제 시간
     func unsetPhoneNumber(user: User, timestamp: Date)
 }
 
@@ -39,6 +51,11 @@ class DefaultPIIEventManager: PIIEventManager {
         track(event: event, user: user, timestamp: timestamp)
     }
     
+    /// secured properties event를 추적
+    /// - Parameters:
+    ///   - event: 이벤트
+    ///   - user: 유저
+    ///   - timestamp: 이벤트 발생 시각
     private func track(event: Event, user: User, timestamp: Date) {
         let hackleUser = userManager.toHackleUser(user: user)
         core.track(event: event, user: hackleUser, timestamp: timestamp)
@@ -46,11 +63,14 @@ class DefaultPIIEventManager: PIIEventManager {
 
 }
 
+/// Personally Identifiable Information Property
 enum PIIProperty: String {
     case phoneNumber = "$phone_number"
 }
 
 extension PropertyOperations {
+    /// property operations를 secured event로 변환
+    /// - Returns: Hackle Event
     fileprivate func toSecuredEvent() -> Event {
         let builder = Event.builder("$secured_properties")
         for (operation, properties) in asDictionary() {
