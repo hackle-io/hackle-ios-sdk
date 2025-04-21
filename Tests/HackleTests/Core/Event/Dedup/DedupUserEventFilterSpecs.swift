ef143788c6e4402c954c258d09f2474f79cc8b47
sungwoo-hackle <sungwoo.yeo@hackle.io>
2025-04-21 15:51:33 +0900

    add test case

--- Tests/HackleTests/Core/Event/Dedup/DedupUserEventFilterSpecs.swift
+++ Tests/HackleTests/Core/Event/Dedup/DedupUserEventFilterSpecs.swift
@@ -35,5 +35,25 @@ class DedupUserEventFilterSpecs: QuickSpec {
             // then
             expect(actual).to(equal(UserEventFilterResult.pass))
         }
+        
+        it("dedup filter return not filtered event") {
+            // given
+            every(eventDedupDeterminer.isDedupTargetMock).returns(true)
+            let event = UserEvents.track("test", user: HackleUser
+                .builder()
+                .properties(["key": "value"])
+                .hackleProperties(["hackleKey": "hackleValue"])
+                .build()
+            )
+            
+            // when
+            let actual = sut.filter(event: event)
+            // then
+            expect(actual.user).to(beIdenticalTo(event.user))
+            expect(actual.timestamp).to(equal(event.timestamp))
+            expect(actual.type).to(equal(event.type))
+            expect(actual.insertId).to(equal(event.insertId))
+        }
     }
 }
+
--- Tests/HackleTests/Core/WebView/WebViewWrapperUserEventFilterSpecs.swift
+++ Tests/HackleTests/Core/WebView/WebViewWrapperUserEventFilterSpecs.swift
@@ -32,12 +32,27 @@ class WebViewWrapperUserEventFilterSpecs: QuickSpec {
             expect(actual).to(equal(UserEventFilterResult.block))
         }
 
-
         it("pass") {
             let sut = WebViewWrapperUserEventFilter()
             let user = HackleUser.builder().identifier(.device, "device").identifier(.hackleDevice, "hackle_device").build()
             let actual = sut.check(event: UserEvents.track("$push_token", user: user))
             expect(actual).to(equal(UserEventFilterResult.pass))
+        }
+        
+        it("webview wrapper filter return filtered event") {
+            // given
+            let sut = WebViewWrapperUserEventFilter()
+            let user = HackleUser.builder().identifier(.device, "device").identifier(.hackleDevice, "hackle_device").build()
+            let event = UserEvents.track("$push_token", user: user)
+            
+            // when
+            let actual = sut.filter(event: event)
+            // then
+            expect(actual.user).toNot(beIdenticalTo(event.user))
+            expect(actual.user.properties.count).to(equal(0))
+            expect(actual.timestamp).to(equal(event.timestamp))
+            expect(actual.type).to(equal(event.type))
+            expect(actual.insertId).to(equal(event.insertId))
         }
     }
 }
