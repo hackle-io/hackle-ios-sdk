c8ccaa704d455cccbbe22177a85602b8f0a27810
sungwoo-hackle <sungwoo.yeo@hackle.io>
2025-05-19 14:07:54 +0900

    wait for appstate active
    - rn에서 앱이 active 되기 전에 userActivity를 실행하면 링크가 rn 사이드로 전달안됨

--- Sources/Hackle/UI/Notification/NotificationHandler.swift
+++ Sources/Hackle/UI/Notification/NotificationHandler.swift
@@ -1,4 +1,5 @@
 import Foundation
+import UIKit
 import UserNotifications
 
 class NotificationHandler {
@@ -60,17 +61,38 @@ extension URL {
         if scheme == "http" || scheme == "https" {
             let userActivity = NSUserActivity(activityType: NSUserActivityTypeBrowsingWeb)
             userActivity.webpageURL = self
-            let success = UIUtils.application?.delegate?.application?(
-                UIUtils.application!,
-                continue: userActivity,
-                restorationHandler: { _ in }
-            )
-            Log.debug("Redirected to: \(self.absoluteString) [success=\(success ?? false)]")
-
+            
+            // NOTE: RN에서 application State가 inactive일 때 userActivity를 처리하면 RN으로 링크가 전달되지 않음
+            //  NotificationCenter에서 UIApplication.didBecomeActiveNotification을 구독하고 active 된 후에 처리
+            switch UIUtils.application?.applicationState {
+            case .active, .background:
+                continueUserActivity(userActivity: userActivity)
+            default:
+                var observer: NSObjectProtocol?
+                observer = NotificationCenter.default.addObserver(
+                    forName: UIApplication.didBecomeActiveNotification,
+                    object: nil,
+                    queue: .main
+                ) { [observer] _ in
+                    self.continueUserActivity(userActivity: userActivity)
+                    if let obs = observer {
+                        NotificationCenter.default.removeObserver(obs)
+                    }
+                }
+            }
         } else {
             UIUtils.application?.open(self, options: [:]) { success in
                 Log.debug("Redirected to: \(self.absoluteString) [success=\(success)]")
             }
         }
+    }
+    
+    private func continueUserActivity(userActivity: NSUserActivity) {
+        let success = UIUtils.application?.delegate?.application?(
+            UIUtils.application!,
+            continue: userActivity,
+            restorationHandler: { _ in }
+        )
+        Log.debug("Redirected to: \(self.absoluteString) [success=\(success ?? false)]")
     }
 }
