772524a86d41d52532dd2d484c4047f3feacedde
sungwoo-hackle <sungwoo.yeo@hackle.io>
2025-05-20 14:18:29 +0900

    apply review

--- Sources/Hackle/UI/Notification/NotificationHandler.swift
+++ Sources/Hackle/UI/Notification/NotificationHandler.swift
@@ -67,7 +67,7 @@ extension URL {
             let userActivity = NSUserActivity(activityType: NSUserActivityTypeBrowsingWeb)
             userActivity.webpageURL = self
             
-            // NOTE: RN에서 application State가 inactive일 때 userActivity를 처리하면 RN으로 링크가 전달되지 않음
+            // NOTE: RN/Flutter에서 application State가 inactive일 때 userActivity를 처리하면 RN으로 링크가 전달되지 않음
             //  NotificationCenter에서 UIApplication.didBecomeActiveNotification을 구독하고 active 된 후에 처리
             switch UIUtils.application?.applicationState {
             case .active, .background:
@@ -86,9 +86,7 @@ extension URL {
                 }
             }
         } else {
-            UIUtils.application?.open(self, options: [:]) { success in
-                Log.debug("Redirected to: \(self.absoluteString) [success=\(success)]")
-            }
+            self.openUrl()
         }
     }
     
@@ -98,6 +96,17 @@ extension URL {
             continue: userActivity,
             restorationHandler: { _ in }
         )
-        Log.debug("Redirected to: \(self.absoluteString) [success=\(success ?? false)]")
+        Log.debug("Redirected to universal link: \(self.absoluteString) [success=\(success ?? false)]")
+        
+        if success != true {
+            Log.info("Attempt to open URL alternative")
+            self.openUrl()
+        }
+    }
+    
+    private func openUrl() {
+        UIUtils.application?.open(self, options: [:]) { success in
+            Log.debug("Redirected to: \(self.absoluteString) [success=\(success)]")
+        }
     }
 }
