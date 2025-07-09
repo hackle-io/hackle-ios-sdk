1e1e216816d62b7a29d15e51ab4d10aec494ffd1
sungwoo-hackle <sungwoo.yeo@hackle.io>
2025-07-09 11:55:53 +0900

    add test case

--- Tests/HackleTests/Core/Event/DefaultUserEventDispatcherSepc.swift
+++ Tests/HackleTests/Core/Event/DefaultUserEventDispatcherSepc.swift
@@ -160,5 +160,130 @@ class DefaultUserEventDispatcherSpec: QuickSpec {
                 expect(count) == 1
             }
         }
+        
+        describe("retry") {
+            it("이벤트 전송 성공 시 next flush allow data는 nil이다") {
+                every(httpClient.executeMock).returns(())
+                
+                let response = mockResponse(statusCode: 200)
+                sut.dispatch(events: eventEntities)
+                httpQueue.sync {
+                }
+                
+                httpClient.executeMock.firstInvokation().arguments.1(response)
+                eventQueue.sync {
+                }
+                
+                expect(sut.nextFlushAllowDate).to(beNil())
+            }
+            
+            it("이벤트 전송 시 에러가 발생하면 next flush allow date가 생성되고, 60초 후로 설정된다") {
+                every(httpClient.executeMock).returns(())
+
+                let response = mockResponse(statusCode: 500)
+                
+                sut.dispatch(events: eventEntities)
+                httpQueue.sync {
+                }
+
+                httpClient.executeMock.firstInvokation().arguments.1(response)
+                eventQueue.sync {
+                }
+                
+                expect(sut.nextFlushAllowDate).toNot(beNil())
+                expect(sut.nextFlushAllowDate) <= Date(timeIntervalSinceNow: 60).timeIntervalSince1970
+            }
+            
+            it("이벤트 전송 시 에러가 발생하면 next flush allow date는 지수백오프 방식으로 증가한다.") {
+                every(httpClient.executeMock).returns(())
+
+                let response = mockResponse(statusCode: 500)
+                
+                var prevInterval: TimeInterval = 0
+                // 60, 120, 240, 480
+                for count in 0..<4 {
+                    sut.dispatch(events: eventEntities)
+                    httpQueue.sync {
+                    }
+                    
+                    httpClient.executeMock.firstInvokation().arguments.1(response)
+                    eventQueue.sync {
+                    }
+                    
+                    expect(sut.nextFlushAllowDate).toNot(beNil())
+                    
+                    let internval = pow(2.0, Double(count)) * 60
+                    let expectedInterval = Date(timeIntervalSinceNow: internval).timeIntervalSince1970
+                    expect(sut.nextFlushAllowDate) <= expectedInterval
+                    expect(sut.nextFlushAllowDate) > prevInterval
+                    prevInterval = expectedInterval
+                }
+            }
+            
+            it("이벤트 전송 시 에러가 발생할 때마다 next flush allow date가 증가하고 최대는 600초(10분)로 제한된다.") {
+                every(httpClient.executeMock).returns(())
+
+                let response = mockResponse(statusCode: 500)
+                
+                // 60, 120, 240, 480
+                for _ in 0..<4 {
+                    sut.dispatch(events: eventEntities)
+                    httpQueue.sync {
+                    }
+                    
+                    httpClient.executeMock.firstInvokation().arguments.1(response)
+                    eventQueue.sync {
+                    }
+                    expect(sut.nextFlushAllowDate).toNot(beNil())
+                }
+                // 600, 600, 600, 600, 600
+                for _ in 0..<5 {
+                    sut.dispatch(events: eventEntities)
+                    httpQueue.sync {
+                    }
+                    
+                    httpClient.executeMock.firstInvokation().arguments.1(response)
+                    eventQueue.sync {
+                    }
+                    
+                    expect(sut.nextFlushAllowDate).toNot(beNil())
+                    
+                    let prevInterval = Date(timeIntervalSinceNow: 480).timeIntervalSince1970
+                    let expectedInterval = Date(timeIntervalSinceNow: 600).timeIntervalSince1970
+                    expect(sut.nextFlushAllowDate) <= expectedInterval
+                    expect(sut.nextFlushAllowDate) > prevInterval
+                }
+            }
+            
+            it("이벤트 전송을 실패하다 전송 성공 시 next flush allow date는 nil 이다") {
+                every(httpClient.executeMock).returns(())
+
+                var response = mockResponse(statusCode: 500)
+                
+                // 60, 120, 240, 480, 600
+                for _ in 0..<5 {
+                    sut.dispatch(events: eventEntities)
+                    httpQueue.sync {
+                    }
+                    
+                    httpClient.executeMock.firstInvokation().arguments.1(response)
+                    eventQueue.sync {
+                    }
+                    
+                    expect(sut.nextFlushAllowDate).toNot(beNil())
+                }
+                
+                response = mockResponse(statusCode: 200)
+                sut.dispatch(events: eventEntities)
+                httpQueue.sync {
+                }
+                
+                httpClient.executeMock.firstInvokation().arguments.1(response)
+                eventQueue.sync {
+                }
+                
+                expect(sut.nextFlushAllowDate).to(beNil())
+            }
+        }
     }
 }
