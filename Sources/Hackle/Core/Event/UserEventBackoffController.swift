6b8dcbdc002eca436e38fd0343c4becfe9272888
sungwoo-hackle <sungwoo.yeo@hackle.io>
2025-07-16 11:14:06 +0900

    apply review

--- Sources/Hackle/Core/Event/UserEventBackoffController.swift
+++ Sources/Hackle/Core/Event/UserEventBackoffController.swift
@@ -15,8 +15,9 @@ protocol UserEventBackoffController {
 class DefaultUserEventBackoffController: UserEventBackoffController {
     private let userEventRetryInterval: TimeInterval
     private let clock: Clock
-    private var nextFlushAllowDate: AtomicReference<Date?> = AtomicReference(value: nil)
-    private var failureCount: AtomicUInt64 = AtomicUInt64(value: 0)
+    private let lock = ReadWriteLock(label: "io.hackle.DefaultUserEventBackoffController.Lock")
+    private var nextFlushAllowDate: Date? = nil
+    private var failureCount: UInt = 0
     
     init(userEventRetryInterval: TimeInterval, clock: Clock) {
         self.userEventRetryInterval = userEventRetryInterval
@@ -24,31 +25,35 @@ class DefaultUserEventBackoffController: UserEventBackoffController {
     }
     
     func checkResponse(_ isSuccess: Bool) {
-        let count = isSuccess ? failureCount.setAndGet(0) : failureCount.addAndGet(1)
-        calculateNextFlushDate(failureCount: count)
+        lock.write {
+            failureCount = isSuccess ? 0 : failureCount + 1
+            calculateNextFlushDate()
+        }
     }
     
     func isAllowNextFlush() -> Bool {
-        guard let nextFlushAllowDate = nextFlushAllowDate.get() else {
+        lock.read {
+            guard let nextFlushAllowDate = nextFlushAllowDate else {
+                return true
+            }
+            
+            let now = clock.now()
+            if now < nextFlushAllowDate {
+                Log.debug("Skipping flush. Next flush date: \(nextFlushAllowDate), current time: \(now)")
+                return false
+            }
+            
             return true
         }
-        
-        let now = clock.now()
-        if now < nextFlushAllowDate {
-            Log.debug("Skipping flush. Next flush date: \(nextFlushAllowDate), current time: \(now)")
-            return false
-        }
-        
-        return true
     }
     
-    private func calculateNextFlushDate(failureCount: UInt64) {
+    private func calculateNextFlushDate() {
         if failureCount == 0 {
-            nextFlushAllowDate.set(newValue: nil)
+            nextFlushAllowDate = nil
         } else {
             let exponential = pow(2.0, Double(failureCount) - 1)
             let intervalSeconds = min(exponential * userEventRetryInterval, userEventRetryMaxInterval)
-            nextFlushAllowDate.set(newValue: clock.now().addingTimeInterval(intervalSeconds))
+            nextFlushAllowDate = clock.now().addingTimeInterval(intervalSeconds)
         }
     }
 }
