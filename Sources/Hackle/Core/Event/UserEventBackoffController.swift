f97c724d65915cee1c67278ba0e79d5cb1fc7012
sungwoo-hackle <sungwoo.yeo@hackle.io>
2025-07-22 11:17:15 +0900

    add network retry (#308)
    * add timestamp column in event
    
    * add next flush date
    
    * add test case
    
    * flush when eventFlushThreshold
    
    * fix test error
    
    * Revert "add timestamp column in event"
    
    This reverts commit 45b644a0a227cbf3780f2bf3f7b127960b11bd09.
    
    * add eventBackoffController
    
    * add test case
    
    * add delete expired events, unify time unit
    
    * add test case
    
    * add mock expired event for test
    
    * add atomic number
    
    * apply review
    
    * modify nextFlushAllowDate type
    
    * apply review
    
    * apply review
    
    * Revert "add atomic number"
    
    This reverts commit 342c7fcdeab7e88be3f85b6add6294551c163b5e.

---
+++ Sources/Hackle/Core/Event/UserEventBackoffController.swift
@@ -0,0 +1,60 @@
+//
+//  UserEventBackoffController.swift
+//  Hackle
+//
+//  Created by sungwoo.yeo on 7/11/25.
+//
+
+import Foundation
+
+protocol UserEventBackoffController {
+    func checkResponse(_ isSuccess: Bool)
+    func isAllowNextFlush() -> Bool
+}
+
+class DefaultUserEventBackoffController: UserEventBackoffController {
+    private let userEventRetryInterval: TimeInterval
+    private let clock: Clock
+    private let lock = ReadWriteLock(label: "io.hackle.DefaultUserEventBackoffController.Lock")
+    private var nextFlushAllowDate: Date? = nil
+    private var failureCount: UInt = 0
+    
+    init(userEventRetryInterval: TimeInterval, clock: Clock) {
+        self.userEventRetryInterval = userEventRetryInterval
+        self.clock = clock
+    }
+    
+    func checkResponse(_ isSuccess: Bool) {
+        lock.write {
+            failureCount = isSuccess ? 0 : failureCount + 1
+            calculateNextFlushDate()
+        }
+    }
+    
+    func isAllowNextFlush() -> Bool {
+        lock.read {
+            guard let nextFlushAllowDate = nextFlushAllowDate else {
+                return true
+            }
+            
+            let now = clock.now()
+            if now < nextFlushAllowDate {
+                Log.debug("Skipping flush. Next flush date: \(nextFlushAllowDate), current time: \(now)")
+                return false
+            }
+            
+            return true
+        }
+    }
+    
+    private func calculateNextFlushDate() {
+        if failureCount == 0 {
+            nextFlushAllowDate = nil
+        } else {
+            let exponential = pow(2.0, Double(failureCount) - 1)
+            let intervalSeconds = min(exponential * userEventRetryInterval, userEventRetryMaxInterval)
+            nextFlushAllowDate = clock.now().addingTimeInterval(intervalSeconds)
+        }
+    }
+}
+
