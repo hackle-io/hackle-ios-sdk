978e689e6f48f88e52b3521571dfaf33e2c70286
sungwoo-hackle <sungwoo.yeo@hackle.io>
2026-01-13 16:45:37 +0900

    add test

--- Tests/HackleTests/HackleConfigSpec.swift
+++ Tests/HackleTests/HackleConfigSpec.swift
@@ -75,5 +75,34 @@ class HackleConfigSpec: QuickSpec {
                 expect(HackleAppMode.web_view_wrapper.description).to(equal("web_view_wrapper"))
             }
         }
+
+        describe("automaticAppLifecycleTracking") {
+            it("default value should be true") {
+                let config = HackleConfigBuilder().build()
+                expect(config.automaticAppLifecycleTracking) == true
+            }
+
+            it("should be able to set to false") {
+                let config = HackleConfigBuilder()
+                    .automaticAppLifecycleTracking(false)
+                    .build()
+                expect(config.automaticAppLifecycleTracking) == false
+            }
+
+            it("should be able to set to true explicitly") {
+                let config = HackleConfigBuilder()
+                    .automaticAppLifecycleTracking(true)
+                    .build()
+                expect(config.automaticAppLifecycleTracking) == true
+            }
+
+            it("last value should take precedence when called multiple times") {
+                let config = HackleConfigBuilder()
+                    .automaticAppLifecycleTracking(false)
+                    .automaticAppLifecycleTracking(true)
+                    .build()
+                expect(config.automaticAppLifecycleTracking) == true
+            }
+        }
     }
 }
