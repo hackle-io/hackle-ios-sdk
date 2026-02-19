78ed2bdbda9ff91e6ffd8e6a9733111c38a0f45e
sungwoo-hackle <sungwoo.yeo@hackle.io>
2026-02-19 13:38:50 +0900

    Apply @MainActor  to inapp message protocol (#379)
    * add @MainActor to HackleInAppMessageView.dismiss()
    
    * add @MainActor to InAppMessageView protocol and extensions
    
    * add @MainActor to InAppMessageActionHandler.handle()
    
    * add @MainActor to InAppMessageEventHandler.handle()
    
    * add @MainActor to InAppMessageEventProcessor.process()
    
    * apply @MainActor at protocol level for InAppMessageView and InAppMessageViewLifecycleListener

--- Tests/HackleTests/UI/InAppMessageUI/Event/InAppMessageActionHandlerSpecs.swift
+++ Tests/HackleTests/UI/InAppMessageUI/Event/InAppMessageActionHandlerSpecs.swift
@@ -21,8 +21,10 @@ class InAppMessageActionHandlerSpecs: QuickSpec {
 
             it("handle close") {
                 let view = MockInAppMessageView(presented: true)
-                sut.handle(view: view, action: InAppMessage.action(type: .close))
-                expect(view.presented) == false
+                MainActor.assumeIsolated {
+                    sut.handle(view: view, action: InAppMessage.action(type: .close))
+                    expect(view.presented) == false
+                }
             }
         }
 
@@ -49,7 +51,9 @@ class InAppMessageActionHandlerSpecs: QuickSpec {
                 let action = InAppMessage.action(type: .webLink, value: nil)
 
                 // when
-                sut.handle(view: view, action: action)
+                MainActor.assumeIsolated {
+                    sut.handle(view: view, action: action)
+                }
 
                 // then - 비동기 작업이 실행될 시간을 준 후 호출되지 않음을 확인
                 RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.1))
@@ -62,7 +66,9 @@ class InAppMessageActionHandlerSpecs: QuickSpec {
                 let action = InAppMessage.action(type: .webLink, value: "")
 
                 // when
-                sut.handle(view: view, action: action)
+                MainActor.assumeIsolated {
+                    sut.handle(view: view, action: action)
+                }
 
                 // then - guard에서 early return되므로 Task 자체가 생성되지 않음
                 RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.1))
@@ -75,7 +81,9 @@ class InAppMessageActionHandlerSpecs: QuickSpec {
                 let action = InAppMessage.action(type: .webLink, value: "https://www.hackle.io")
 
                 // when
-                sut.handle(view: view, action: action)
+                MainActor.assumeIsolated {
+                    sut.handle(view: view, action: action)
+                }
 
                 // then - toEventually로 비동기 호출 대기
                 expect(urlHandler.openCallCount).toEventually(equal(1), timeout: .seconds(1))
@@ -106,12 +114,16 @@ class InAppMessageActionHandlerSpecs: QuickSpec {
                 let action = InAppMessage.action(type: .linkAndClose, value: nil)
 
                 // when
-                sut.handle(view: view, action: action)
+                MainActor.assumeIsolated {
+                    sut.handle(view: view, action: action)
+                }
 
                 // then - guard에서 early return되므로 dismiss도 호출되지 않음
                 RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.1))
                 expect(urlHandler.openCallCount).to(equal(0))
-                expect(view.presented).to(beTrue())
+                MainActor.assumeIsolated {
+                    expect(view.presented).to(beTrue())
+                }
             }
 
             it("when invalid url then do nothing") {
@@ -120,12 +132,16 @@ class InAppMessageActionHandlerSpecs: QuickSpec {
                 let action = InAppMessage.action(type: .linkAndClose, value: "")
 
                 // when
-                sut.handle(view: view, action: action)
+                MainActor.assumeIsolated {
+                    sut.handle(view: view, action: action)
+                }
 
                 // then - guard에서 early return
                 RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.1))
                 expect(urlHandler.openCallCount).to(equal(0))
-                expect(view.presented).to(beTrue())
+                MainActor.assumeIsolated {
+                    expect(view.presented).to(beTrue())
+                }
             }
 
             it("hackle link and close") {
@@ -134,10 +150,12 @@ class InAppMessageActionHandlerSpecs: QuickSpec {
                 let action = InAppMessage.action(type: .linkAndClose, value: "https://www.hackle.io")
 
                 // when
-                sut.handle(view: view, action: action)
+                MainActor.assumeIsolated {
+                    sut.handle(view: view, action: action)
 
-                // then - view.dismiss()는 동기적으로 호출되고, urlHandler.open은 비동기로 호출됨
-                expect(view.presented).to(beFalse())
+                    // then - view.dismiss()는 동기적으로 호출되고, urlHandler.open은 비동기로 호출됨
+                    expect(view.presented).to(beFalse())
+                }
                 expect(urlHandler.openCallCount).toEventually(equal(1), timeout: .seconds(1))
                 expect(urlHandler.lastOpenedUrl?.absoluteString).to(equal("https://www.hackle.io"))
             }
@@ -167,7 +185,9 @@ class InAppMessageActionHandlerSpecs: QuickSpec {
                 let action = InAppMessage.action(type: .hidden)
 
                 // when
-                sut.handle(view: view, action: action)
+                MainActor.assumeIsolated {
+                    sut.handle(view: view, action: action)
+                }
 
                 // then
                 expect(repository.getDouble(key: "42")) == (60 * 60 * 24) + 42
@@ -180,7 +200,9 @@ class InAppMessageActionHandlerSpecs: QuickSpec {
                 let action = InAppMessage.action(type: .hidden)
 
                 // when
-                sut.handle(view: view, action: action)
+                MainActor.assumeIsolated {
+                    sut.handle(view: view, action: action)
+                }
 
                 // then
                 expect(repository.getDouble(key: "42")) == 0.0 // not saved
