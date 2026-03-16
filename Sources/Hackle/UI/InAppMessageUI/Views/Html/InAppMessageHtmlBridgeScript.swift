105b35d41eeaa4d5efd1763f9fc74a5929ee7f11
yong <yong@hackle.io>
2026-03-16 19:29:11 +0900

    [HACKLE-13753] Add: InAppMessageView HtmlScript

---
+++ Sources/Hackle/UI/InAppMessageUI/Views/Html/InAppMessageHtmlBridgeScript.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+class InAppMessageHtmlBridgeScript: WebViewUserScript {
+    private let url: String
+    
+    init(url: String) {
+        self.url = url
+    }
+    
+    var name: String {
+        return "InAppMessageHtmlBridgeScript"
+    }
+    
+    var source: String {
+        return """
+        (function() {
+            var s = document.createElement('script');
+            s.src = '\(url)';
+            s.onload = function() {
+                Hackle.setWebAppInAppMessageHtmlBridge();
+            };
+            document.head.appendChild(s);
+        })();
+        """
+    }
+}
+
+extension InAppMessageHtmlBridgeScript {
+    private static let javascriptSdkUrlKey = "$javascript_sdk_url"
+    private static let defaultJavaScriptSdkUrl = "https://cdn2.hackle.io/npm/@hackler/javascript-sdk@11.54.0/lib/index.browser.umd.min.js"
+    
+    func create(config: HackleConfig) -> InAppMessageHtmlBridgeScript {
+        let url = config.get(Self.javascriptSdkUrlKey) ?? Self.defaultJavaScriptSdkUrl
+        return InAppMessageHtmlBridgeScript(url: url)
+    }
+}
