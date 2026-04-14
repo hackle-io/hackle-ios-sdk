543a7adb68293c01b33d3c4ec963350b708da172
sungwoo-hackle <sungwoo.yeo@hackle.io>
2026-04-14 17:07:03 +0900

    test: remove WKFrameInfo-dependent tests from HackleUIDelegateSpecs

--- Tests/HackleTests/Invocator/HackleUIDelegateSpecs.swift
+++ Tests/HackleTests/Invocator/HackleUIDelegateSpecs.swift
@@ -13,104 +13,6 @@ class HackleUIDelegateSpecs: QuickSpec {
             mockInvocator = MockInvocator()
         }
 
-        describe("webView runJavaScriptTextInputPanelWithPrompt") {
-
-            it("invocable prompt should invoke and return result via completionHandler") {
-                MainActor.assumeIsolated {
-                    let sut = HackleUIDelegate(invocator: mockInvocator)
-                    mockInvocator.invocable = true
-                    mockInvocator.invokeResult = "{\"result\":\"ok\"}"
-
-                    let webView = WKWebView()
-                    let frame = WKFrameInfo()
-                    var callbackResult: String? = "not_called"
-
-                    sut.webView(
-                        webView,
-                        runJavaScriptTextInputPanelWithPrompt: "test_prompt",
-                        defaultText: nil,
-                        initiatedByFrame: frame
-                    ) { result in
-                        callbackResult = result
-                    }
-
-                    expect(mockInvocator.invokedString) == "test_prompt"
-                    expect(callbackResult) == "{\"result\":\"ok\"}"
-                }
-            }
-
-            it("non-invocable prompt without uiDelegate should call completionHandler with nil") {
-                MainActor.assumeIsolated {
-                    let sut = HackleUIDelegate(invocator: mockInvocator, uiDelegate: nil)
-                    mockInvocator.invocable = false
-
-                    let webView = WKWebView()
-                    let frame = WKFrameInfo()
-                    var callbackResult: String? = "not_called"
-
-                    sut.webView(
-                        webView,
-                        runJavaScriptTextInputPanelWithPrompt: "non_hackle_prompt",
-                        defaultText: nil,
-                        initiatedByFrame: frame
-                    ) { result in
-                        callbackResult = result
-                    }
-
-                    expect(callbackResult).to(beNil())
-                }
-            }
-
-            it("non-invocable prompt with uiDelegate should forward to uiDelegate") {
-                MainActor.assumeIsolated {
-                    let mockUIDelegate = MockWKUIDelegate()
-                    let sut = HackleUIDelegate(invocator: mockInvocator, uiDelegate: mockUIDelegate)
-                    mockInvocator.invocable = false
-
-                    let webView = WKWebView()
-                    let frame = WKFrameInfo()
-                    var callbackResult: String? = "not_called"
-
-                    sut.webView(
-                        webView,
-                        runJavaScriptTextInputPanelWithPrompt: "external_prompt",
-                        defaultText: "default",
-                        initiatedByFrame: frame
-                    ) { result in
-                        callbackResult = result
-                    }
-
-                    expect(mockUIDelegate.promptCalled) == true
-                    expect(mockUIDelegate.receivedPrompt) == "external_prompt"
-                    expect(mockUIDelegate.receivedDefaultText) == "default"
-                    expect(callbackResult) == "delegated_result"
-                }
-            }
-
-            it("non-invocable prompt with uiDelegate that does not implement prompt method should call completionHandler with nil") {
-                MainActor.assumeIsolated {
-                    let minimalDelegate = MinimalWKUIDelegate()
-                    let sut = HackleUIDelegate(invocator: mockInvocator, uiDelegate: minimalDelegate)
-                    mockInvocator.invocable = false
-
-                    let webView = WKWebView()
-                    let frame = WKFrameInfo()
-                    var callbackResult: String? = "not_called"
-
-                    sut.webView(
-                        webView,
-                        runJavaScriptTextInputPanelWithPrompt: "some_prompt",
-                        defaultText: nil,
-                        initiatedByFrame: frame
-                    ) { result in
-                        callbackResult = result
-                    }
-
-                    expect(callbackResult).to(beNil())
-                }
-            }
-        }
-
         describe("responds(to:)") {
 
             it("should return true for selectors HackleUIDelegate responds to") {
@@ -176,30 +78,6 @@ class HackleUIDelegateSpecs: QuickSpec {
                     expect(target).to(beNil())
                 }
             }
-
-            it("should call completionHandler with nil after uiDelegate is deallocated") {
-                MainActor.assumeIsolated {
-                    var mockDelegate: MockWKUIDelegate? = MockWKUIDelegate()
-                    let sut = HackleUIDelegate(invocator: mockInvocator, uiDelegate: mockDelegate)
-                    mockInvocator.invocable = false
-                    mockDelegate = nil
-
-                    let webView = WKWebView()
-                    let frame = WKFrameInfo()
-                    var callbackResult: String? = "not_called"
-
-                    sut.webView(
-                        webView,
-                        runJavaScriptTextInputPanelWithPrompt: "prompt",
-                        defaultText: nil,
-                        initiatedByFrame: frame
-                    ) { result in
-                        callbackResult = result
-                    }
-
-                    expect(callbackResult).to(beNil())
-                }
-            }
         }
 
         describe("forwardingTarget(for:)") {
@@ -261,13 +139,6 @@ private class MockWKUIDelegate: NSObject, WKUIDelegate {
     var promptCalled = false
     var receivedPrompt: String?
     var receivedDefaultText: String?
-
-    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping @MainActor @Sendable (String?) -> Void) {
-        promptCalled = true
-        receivedPrompt = prompt
-        receivedDefaultText = defaultText
-        completionHandler("delegated_result")
-    }
 
     @objc func customMethod() {}
 }
