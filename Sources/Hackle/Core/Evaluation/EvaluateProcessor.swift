425b22513e7b330b405b47ec3ed781d4084b6344
sungwoo-hackle <sungwoo.yeo@hackle.io>
2026-07-01 11:16:04 +0900

    feat: add EvaluateProcessor facade shell with single record trigger

---
+++ Sources/Hackle/Core/Evaluation/EvaluateProcessor.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+class EvaluateProcessor {
+
+    private let evaluatorFactory: EvaluatorFactory
+
+    init(evaluatorFactory: EvaluatorFactory) {
+        self.evaluatorFactory = evaluatorFactory
+    }
+
+    // 타입드 진입점(experiment/remoteConfig/inAppMessage x2)은 step 3/4/5에서 추가.
+    // static create(...)는 step 6에서 추가.
+
+    fileprivate func evaluate<Res: EvaluateResponse>(evaluator: any Evaluator, request: EvaluateRequest) throws -> Res {
+        let response: Res = try evaluator.evaluate(request: request, context: Evaluators.context())
+        if request.record {
+            evaluator.record(request: request, response: response)
+        }
+        return response
+    }
+}
