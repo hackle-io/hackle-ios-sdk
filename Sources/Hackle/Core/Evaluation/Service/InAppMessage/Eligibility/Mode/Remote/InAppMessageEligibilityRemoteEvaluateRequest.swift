9504593f8be2c543b8f22c9f624931e6a4d199fe
sungwoo-hackle <sungwoo.yeo@hackle.io>
2026-07-09 11:15:33 +0900

    feat: add in-app message eligibility remote evaluate request

---
+++ Sources/Hackle/Core/Evaluation/Service/InAppMessage/Eligibility/Mode/Remote/InAppMessageEligibilityRemoteEvaluateRequest.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+final class InAppMessageEligibilityRemoteEvaluateRequest: RemoteEvaluateRequest, InAppMessageEligibilityEvaluateRequest, CustomStringConvertible {
+
+    let evaluationWorkspace: WorkspaceEvaluation
+    let result: InAppMessageEligibilityRemoteEvaluateResult
+    let user: HackleUser
+    let record: Bool
+    let scope: InAppMessageEvaluateScope
+    let platformType: PlatformType
+    let timestamp: Date
+
+    var workspace: Workspace { evaluationWorkspace }
+    var entity: Entity { result }
+    var remoteResult: RemoteEvaluateResult { result }
+    var inAppMessage: InAppMessage { result }
+
+    private init(
+        workspace: WorkspaceEvaluation,
+        entity: InAppMessageEligibilityRemoteEvaluateResult,
+        user: HackleUser,
+        record: Bool,
+        scope: InAppMessageEvaluateScope,
+        platformType: PlatformType,
+        timestamp: Date
+    ) {
+        self.evaluationWorkspace = workspace
+        self.result = entity
+        self.user = user
+        self.record = record
+        self.scope = scope
+        self.platformType = platformType
+        self.timestamp = timestamp
+    }
+
+    var description: String {
+        "InAppMessageEligibilityRemoteEvaluateRequest(type=IN_APP_MESSAGE, key=\(result.key))"
+    }
+
+    static func of(
+        workspace: WorkspaceEvaluation,
+        entity: InAppMessageEligibilityRemoteEvaluateResult,
+        user: HackleUser,
+        scope: InAppMessageEvaluateScope,
+        platformType: PlatformType,
+        timestamp: Date,
+        record: Bool = true
+    ) -> InAppMessageEligibilityRemoteEvaluateRequest {
+        InAppMessageEligibilityRemoteEvaluateRequest(
+            workspace: workspace,
+            entity: entity,
+            user: user,
+            record: record,
+            scope: scope,
+            platformType: platformType,
+            timestamp: timestamp
+        )
+    }
+}
