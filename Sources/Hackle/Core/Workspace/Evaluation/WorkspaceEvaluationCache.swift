13c767336a96d5c17f33b6be6bd8a0095e797a8e
sungwoo-hackle <sungwoo.yeo@hackle.io>
2026-07-16 17:04:53 +0900

    apply recursiveLock

--- Sources/Hackle/Core/Workspace/Evaluation/WorkspaceEvaluationCache.swift
+++ Sources/Hackle/Core/Workspace/Evaluation/WorkspaceEvaluationCache.swift
@@ -10,7 +10,7 @@ protocol WorkspaceEvaluationCache {
 class LruWorkspaceEvaluationCache: WorkspaceEvaluationCache {
 
     private let capacity: Int
-    private let lock = NSLock()
+    private let recursiveLock = RecursiveLock(label: "io.hackle.LruWorkspaceEvaluationCache")
     private var entries = [WorkspaceEvaluationContext.Key: WorkspaceEvaluationContext]()
     private var order = [WorkspaceEvaluationContext.Key]()
 
@@ -19,14 +19,15 @@ class LruWorkspaceEvaluationCache: WorkspaceEvaluationCache {
     }
 
     func get(key: WorkspaceEvaluationContext.Key) -> WorkspaceEvaluationContext? {
-        lock.lock()
-        defer { lock.unlock() }
-        return entries[key]
+        recursiveLock.lock {
+            return entries[key]
+        }
+        
     }
 
     func put(context: WorkspaceEvaluationContext) -> [WorkspaceEvaluationContext] {
-        let lastEvaluatedAt = get(key: context.key)?.dto.metadata.evaluatedAt ?? Int64.min
-        return safeLock.lock {
+        return recursiveLock.lock {
+            let lastEvaluatedAt = get(key: context.key)?.dto.metadata.evaluatedAt ?? Int64.min
             if lastEvaluatedAt <= context.dto.metadata.evaluatedAt {
                 remove(key: context.key)
                 add(context: context)
@@ -40,21 +41,21 @@ class LruWorkspaceEvaluationCache: WorkspaceEvaluationCache {
     }
 
     func latest() -> WorkspaceEvaluationContext? {
-        lock.lock()
-        defer { lock.unlock() }
-        guard let key = order.last else {
-            return nil
+        recursiveLock.lock {
+            guard let key = order.last else {
+                return nil
+            }
+            return entries[key]
         }
-        return entries[key]
     }
 
     func restore(contexts: [WorkspaceEvaluationContext]) {
-        lock.lock()
-        defer { lock.unlock() }
-        entries.removeAll()
-        order.removeAll()
-        for context in contexts.suffix(capacity) {
-            add(context: context)
+        recursiveLock.lock {
+            entries.removeAll()
+            order.removeAll()
+            for context in contexts.suffix(capacity) {
+                add(context: context)
+            }
         }
     }
 
