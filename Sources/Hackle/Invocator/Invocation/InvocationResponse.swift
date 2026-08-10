921cf74f3ca0d51ef380a054cc0a34555ea9f849
sungwoo-hackle <sungwoo.yeo@hackle.io>
2026-08-10 10:58:56 +0900

    feat: carry user mutation completion task in invocation response

--- Sources/Hackle/Invocator/Invocation/InvocationResponse.swift
+++ Sources/Hackle/Invocator/Invocation/InvocationResponse.swift
@@ -4,19 +4,25 @@ struct InvocationResponse<T> {
     let isSuccess: Bool
     let message: String
     let data: T?
+    /// 비동기 처리의 완료 신호. user mutation 명령만 값을 가지며 직렬화되지 않는다.
+    let task: Task<Void, Never>?
 }
 
 extension InvocationResponse {
     static func success() -> InvocationResponse<T> {
-        return .init(isSuccess: true, message: "OK", data: nil)
+        return .init(isSuccess: true, message: "OK", data: nil, task: nil)
+    }
+
+    static func success(task: Task<Void, Never>) -> InvocationResponse<T> {
+        return .init(isSuccess: true, message: "OK", data: nil, task: task)
     }
 
     static func success(data: T) -> InvocationResponse<T> {
-        return .init(isSuccess: true, message: "OK", data: data)
+        return .init(isSuccess: true, message: "OK", data: data, task: nil)
     }
 
     static func error(error: Error) -> InvocationResponse<T> {
-        return .init(isSuccess: false, message: error.localizedDescription, data: nil)
+        return .init(isSuccess: false, message: error.localizedDescription, data: nil, task: nil)
     }
 }
 
